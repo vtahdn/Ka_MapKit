5140537467f7766343f20c2811b5c250955d8593
Viet Asc <vtahdn@gmail.com>
2019-01-10 16:26:08 +0700

    Initial Commit

---
+++ Ka_MapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ka_MapKit
+//
+//  Created by Viet Asc on 1/10/19.
+//  Copyright © 2019 Viet Asc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Ka_MapKitTests/Ka_MapKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ka_MapKitTests.swift
+//  Ka_MapKitTests
+//
+//  Created by Viet Asc on 1/10/19.
+//  Copyright © 2019 Viet Asc. All rights reserved.
+//
+
+import XCTest
+@testable import Ka_MapKit
+
+class Ka_MapKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
